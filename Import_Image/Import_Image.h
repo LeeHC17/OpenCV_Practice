87a41dfae9ee1841e7c6f79c109807efc5e66c5c
LeeHC17 <lhchang9@naver.com>
2021-07-12 17:32:52 +0900

    update 7/12

---
+++ Import_Image/Import_Image.h
@@ -0,0 +1,32 @@
+﻿
+// Import_Image.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CImportImageApp:
+// 이 클래스의 구현에 대해서는 Import_Image.cpp을(를) 참조하세요.
+//
+
+class CImportImageApp : public CWinApp
+{
+public:
+	CImportImageApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CImportImageApp theApp;
