9ad06ea956697b80fb1ad43300e3c97ebd1b0a92
LeeHC17 <lhchang9@naver.com>
2021-07-19 17:43:02 +0900

    update 7/19

---
+++ Contour/ContourDlg.cpp
@@ -0,0 +1,224 @@
+﻿
+// ContourDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Contour.h"
+#include "ContourDlg.h"
+#include "afxdialogex.h"
+#include <iostream>
+#include "opencv2/opencv.hpp"
+using namespace cv;
+using namespace std;;
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CContourDlg 대화 상자
+
+
+
+CContourDlg::CContourDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CONTOUR_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CContourDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CContourDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &CContourDlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CContourDlg 메시지 처리기
+
+BOOL CContourDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CContourDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CContourDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CContourDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+void SetLabel(Mat& img, const vector<Point>& pts, const String& Label) {
+	Rect rc = boundingRect(pts);
+	rectangle(img, rc, Scalar(0, 255, 0), 1);
+	putText(img, Label, rc.tl(), FONT_HERSHEY_PLAIN, 1, Scalar(0, 255, 0));
+}
+
+void CContourDlg::OnBnClickedButton1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	if (src.empty()) {
+		MessageBox(_T("Image load failed!!"), _T("ERROR"), MB_ICONERROR);
+	}
+
+	Mat gray_img;
+	cvtColor(src, gray_img, COLOR_BGR2GRAY);
+
+	Mat binary;
+	threshold(gray_img, binary, 200, 255, THRESH_BINARY_INV | THRESH_OTSU);
+
+	vector<vector<Point>> contours;
+	findContours(binary, contours, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+
+	for (vector<Point>& pts : contours) {
+		if (contourArea(pts) < 400) {
+			continue;
+		}
+
+		vector<Point> approximate;
+		approxPolyDP(pts, approximate, arcLength(pts, true) * 0.02, true);
+
+		int vtc = (int)approximate.size();
+
+		if (vtc == 3) {
+			// triangle
+			SetLabel(src, pts, "Triangle");
+		}
+		else if (vtc == 4) {
+			// rectangle
+			SetLabel(src, pts, "Rectangle");
+		}
+		else if (vtc == 5) {
+			// pentagon
+			SetLabel(src, pts, "Pentagon");
+		}
+		else if (vtc > 5) {
+			//circle
+			double len = arcLength(pts, true);
+			double area = contourArea(pts);
+			double ratio = 4.0 * CV_PI * area / (len * len);
+
+			if (ratio > 0.8) {
+				SetLabel(src, pts, "Circle");
+			}
+			else if (ratio <= 0.8) {
+				SetLabel(src, pts, "Ellipse");
+			}
+		}
+	}
+	
+	imshow("Shapes detecting", src);
+
+	waitKey(0);
+	destroyWindow("Shapes detecting");
+}
---
+++ Contour/ContourDlg.h
@@ -0,0 +1,37 @@
+﻿
+// ContourDlg.h: 헤더 파일
+//
+#include "opencv2/opencv.hpp"
+using namespace cv;
+#pragma once
+
+
+// CContourDlg 대화 상자
+class CContourDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CContourDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CONTOUR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+	Mat src = imread("shapes.png", IMREAD_COLOR);
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+};
